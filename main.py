13127874c99692f0c268f90d68a53ab68ef73845
AMKiasat <kiasatamirm@gmail.com>
2021-11-26 19:10:03 +0330

    stop words removal & positional indexing added

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import codecs
+
 import pandas as pd
 from parsivar import Normalizer
 from parsivar import Tokenizer
@@ -12,22 +14,35 @@ from pandas import ExcelFile
 
 
 def main():
-    normalized = []
+    unique = []
+    position = []
     df = pd.read_excel('IR1_7k_news.xlsx')
     x = df['content']
     my_normalizer = Normalizer(statistical_space_correction=True)
     my_tokenizer = Tokenizer()
     my_stemmer = FindStems()
-    words = []
+    stop_words = codecs.open('Stop_words.txt', encoding='utf-8').read().split('\r\n')
+    print(stop_words)
     stemmed = []
-    for i in x:
-        words = my_tokenizer.tokenize_words(my_normalizer.normalize(my_normalizer.normalize(i)))
+    for i in range(len(x)):
+        words = my_tokenizer.tokenize_words(my_normalizer.normalize(my_normalizer.normalize(x[i])))
         tmp = []
-        for j in words:
-            tmp.append(my_stemmer.convert_to_stem(j))
+        for j in range(len(words)):
+            if stop_words.count(words[j]) == 0:
+                b = my_stemmer.convert_to_stem(words[j])
+                tmp.append(b)
+                if unique.count(b) == 0:
+                    unique.append(b)
+                    tm = [i, j]
+                    t = [tm]
+                    position.append(t)
+                else:
+                    a = unique.index(b)
+                    tm = [i, j]
+                    position[a].append(tm)
         stemmed.append(tmp)
-        print(stemmed.pop())
-    
+    print(unique)
+    print(position)
 
 if __name__ == '__main__':
     main()
