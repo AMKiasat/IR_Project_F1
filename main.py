98950b4345424ee548be720b15e1b7b13ce23b56
AMKiasat <kiasatamirm@gmail.com>
2021-11-26 17:30:22 +0330

    Preprocessing done

--- main.py
+++ main.py
@@ -1,18 +1,39 @@
 import pandas as pd
 from parsivar import Normalizer
 from parsivar import Tokenizer
+from parsivar import SpellCheck
+from parsivar import FindStems
 from pandas import ExcelWriter
 from pandas import ExcelFile
 
+
 # 'IR1_7k_news.xlsx'
 # 'content'
+
+
 def main():
+    normalized = []
     df = pd.read_excel('IR1_7k_news.xlsx')
     x = df['content']
-    my_normalizer = Normalizer()
+    my_normalizer = Normalizer(statistical_space_correction=True)
     my_tokenizer = Tokenizer()
-    words = my_tokenizer.tokenize_words(my_normalizer.normalize(x[0]))
-    print(words)
+    my_stemmer = FindStems()
+
+    for i in x:
+        normalized.append(my_normalizer.normalize(i))
+
+    words = []
+    for i in normalized:
+        words.append(my_tokenizer.tokenize_words(my_normalizer.normalize(i)))
+    # print(words[0])
+
+    stemmed = []
+    for i in range(len(words)):
+        tmp = []
+        for j in words[i]:
+            tmp.append(my_stemmer.convert_to_stem(j))
+        stemmed.append(tmp)
+    print(stemmed[0])
 
 
 if __name__ == '__main__':
